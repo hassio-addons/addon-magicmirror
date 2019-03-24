34bd79be7d7b005c4dadfd41131be236f46852e6
ludeeus <joasoe@gmail.com>
2019-03-24 19:52:29 +0100

    ✨ Add modules

--- magicmirror/rootfs/etc/cont-init.d/20-magicmirror.sh
+++ magicmirror/rootfs/etc/cont-init.d/20-magicmirror.sh
@@ -4,12 +4,15 @@
 # Configures MagicMirror²
 # ==============================================================================
 
+readonly ghurl='https://github.com/'
+readonly modules=$(bashio::config 'modules')
+
 # Initial config
 if ! bashio::fs.file_exists '/config/magicmirror/config.js'; then
     if ! bashio::fs.directory_exists '/config/magicmirror'; then
         mkdir /config/magicmirror
     fi
-    cp /opt/config/config.js.sample /config/magicmirror/config.js
+    cp /opt/config/default.js /config/magicmirror/config.js
 fi
 
 if ! bashio::fs.directory_exists '/data/modules'; then
@@ -28,3 +31,13 @@ fi
 ln -sf /config/magicmirror/config.js /opt/config/config.js
 ln -sf /data/modules /opt/modules
 ln -sf /config/magicmirror/css /opt/css
+
+# Install user defined modules
+
+for module in ${modules}; do
+    bashio::log.info "Installing ${module}"
+    cd /opt/modules || bashio::exit "/opt/modules/ does not exist!"
+    git clone "${ghurl}${module}.git"
+    cd MMM-HASS || bashio::exit "MMM-HASS does not exist!"
+    npm install --unsafe-perm --silent
+done
